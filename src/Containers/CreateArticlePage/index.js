2d86f364435f9f41be56a91cab874ef7a163fdf6
Pradip <pradipshrestha618@gmail.com>
2020-01-30 10:12:40 +0545

    write article page in progress

---
+++ src/Containers/CreateArticlePage/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../../Components/NavBar'
+
+const CreateArticle = () => {
+    return(
+        <>
+        <Navbar/>
+            <div className='create-article-container'>
+                Here We Create An Article...
+            </div>
+        </>
+    )
+};
+
+export default CreateArticle;
